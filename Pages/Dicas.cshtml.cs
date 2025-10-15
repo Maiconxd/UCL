be0a1e28f85295951767e3eecac4a5718bbfcd3a
Mayck Santos <80600589+Maiconxd@users.noreply.github.com>
2025-10-15 15:38:46 -0300

    App do Idoso

---
+++ Pages/Dicas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppIdoso2._0.Pages
+{
+    public class DicasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
